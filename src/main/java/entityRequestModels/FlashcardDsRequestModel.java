17d027236cda076d961649d1675297dff62aeb27
lucasprates74 <79284676+lucasprates74@users.noreply.github.com>
2022-11-03 23:15:38 -0400

    created request model classes

---
+++ src/main/java/entityRequestModels/FlashcardDsRequestModel.java
@@ -0,0 +1,32 @@
+package dataAccess;
+// use case layer
+
+import java.time.LocalDateTime;
+
+public class FlashcardDsRequestModel {
+    private String term;
+    private String definition;
+    private String creationDate;
+    private String flashcardId;
+    private String belongsToId;
+
+    // create request model from database entry
+    public FlashcardDsRequestModel(String term, String definition, String creationDate,
+                                   String flashcardId, String belongsToId){
+        this.term = term;
+        this.definition = definition;
+        this.creationDate = creationDate;
+        this.flashcardId = flashcardId;
+        this.belongsToId = belongsToId;
+    }
+
+    // create request model from in memory flashcard
+    public FlashcardDsRequestModel(String term, String definition, LocalDateTime creationDate,
+                                   int flashcardId, int belongsToId){
+        this.term = term;
+        this.definition = definition;
+        this.creationDate = creationDate.toString();
+        this.flashcardId = Integer.toString(flashcardId);
+        this.belongsToId = Integer.toString(belongsToId);
+    }
+}
---
+++ src/main/java/entityRequestModels/UserDsRequestModel.java
@@ -0,0 +1,2 @@
+package dataAccess;public interface UserDsRequestModel {
+}
