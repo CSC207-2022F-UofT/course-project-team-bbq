e96017c3dcd872c5ac06ca4a7818c0cf10581dc5
Winston Chieng <winston.chieng03@gmail.com>
2022-11-07 12:52:23 -0500

    Implemented Request model for search

--- src/main/java/search_use_case/SearchRequestModel.java
+++ src/main/java/search_use_case/SearchRequestModel.java
@@ -1,6 +1,29 @@
 package search_use_case;
+import java.util.ArrayList;
 
 public class SearchRequestModel {
 
-    // To be implemented
+    private String search_input;
+    private ArrayList<String> tags;
+
+    public SearchRequestModel(String search_input, ArrayList<String> tags){
+        this.search_input = search_input;
+        this.tags = tags;
+    }
+
+    String getSearch_input(){
+        return search_input;
+    }
+    void setSearch_input(String search_input){
+        this.search_input = search_input;
+    }
+
+    ArrayList<String> getTags(){
+        return tags;
+    }
+
+    void setTags(ArrayList<String> tags){
+        this.tags = tags;
+    }
+
 }
