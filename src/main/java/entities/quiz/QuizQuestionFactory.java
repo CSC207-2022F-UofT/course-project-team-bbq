1146f41621a9456d7265c13b670cb76f64a7a99d
topazerino <47570422+topazerino@users.noreply.github.com>
2022-11-08 18:34:52 -0500

    autograding test

--- src/main/java/entities/quiz/QuizQuestionFactory.java
+++ src/main/java/entities/quiz/QuizQuestionFactory.java
@@ -1,15 +1,14 @@
 package entities.quiz;
 
-import entities.flashcard.Flashcard;
 import entities.flashcard.FlashcardSet;
 
 public class QuizQuestionFactory {
-    public QuizQuestion createQuizQuestion(FlashcardSet flashcardSet, int index, String type) {
-        return switch (type) {
-            case "MC" -> new MultipleChoiceQuestion(flashcardSet, index);
-            case "TE" -> new TextEntryQuestion(flashcardSet, index);
-            case "TF" -> new TrueFalseQuestion(flashcardSet, index);
-            default -> throw new IllegalArgumentException("??? what u doing");
-        };
-    }
+//    public QuizQuestion createQuizQuestion(FlashcardSet flashcardSet, int index, String type) {
+//        return switch (type) {
+//            case "MC" -> new MultipleChoiceQuestion(flashcardSet, index);
+//            case "TE" -> new TextEntryQuestion(flashcardSet, index);
+//            case "TF" -> new TrueFalseQuestion(flashcardSet, index);
+//            default -> throw new IllegalArgumentException("wrong");
+//        };
+//    }
 }
