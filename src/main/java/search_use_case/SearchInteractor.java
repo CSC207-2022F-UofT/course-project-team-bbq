9e6eae833e092b314010e46eadbc52d0b8778187
Winston Chieng <winston.chieng03@gmail.com>
2022-11-09 12:06:19 -0500

    Set foundation for SearchInteractor class

--- src/main/java/search_use_case/SearchInteractor.java
+++ src/main/java/search_use_case/SearchInteractor.java
@@ -1,6 +1,18 @@
 package search_use_case;
 
-public class SearchInteractor {
+public class SearchInteractor implements SearchInputBoundary{
 
-    // to be implemented
+    final SearchPresenter presenter;
+
+    // figure out what else needs to be here
+    public SearchInteractor(SearchPresenter presenter){
+        this.presenter = presenter;
+    }
+
+    public SearchResponseModel create(SearchRequestModel requestModel){
+
+        // search algorithm
+
+        // return presenter.prepareSuccessView()
+    }
 }
--- src/main/java/search_use_case/SearchPresenter.java
+++ src/main/java/search_use_case/SearchPresenter.java
@@ -1,6 +1,7 @@
 package search_use_case;
 
 public interface SearchPresenter {
+
     SearchResponseModel prepareSuccessView(SearchResponseModel results);
 
     SearchResponseModel prepareFailView(String error);
