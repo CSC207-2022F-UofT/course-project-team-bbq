01e9732d256c8550e1964456cda25712377f0578
Winston Chieng <winston.chieng03@gmail.com>
2022-11-11 21:24:45 -0500

    modified controller to account for input boundary, set foundation for interactor

--- src/main/java/search_use_case/SearchController.java
+++ src/main/java/search_use_case/SearchController.java
@@ -1,14 +1,18 @@
 package search_use_case;
 
+import java.util.ArrayList;
+
 public class SearchController {
 
-    private final SearchInputBoundary searchInput;
+    private final SearchInputBoundary userInput;
 
-    public SearchController(SearchInputBoundary searchInput){
-        this.searchInput = searchInput;
+    public SearchController(SearchInputBoundary userInput){
+        this.userInput = userInput;
     }
 
-    SearchResponseModel create(SearchRequestModel requestModel){
-        return searchInput.create(requestModel);
+    SearchResponseModel create(String search_input, ArrayList<String> tags){
+        SearchRequestModel requestModel = new SearchRequestModel(search_input, tags);
+
+        return userInput.create(requestModel);
     }
 }
--- src/main/java/search_use_case/SearchInteractor.java
+++ src/main/java/search_use_case/SearchInteractor.java
@@ -1,5 +1,9 @@
 package search_use_case;
 
+import entities.FlashcardSet;
+
+import java.util.ArrayList;
+
 public class SearchInteractor implements SearchInputBoundary{
 
     final SearchPresenter presenter;
@@ -9,10 +13,24 @@ public class SearchInteractor implements SearchInputBoundary{
         this.presenter = presenter;
     }
 
+    @Override
     public SearchResponseModel create(SearchRequestModel requestModel){
 
-        // search algorithm
+        ArrayList<FlashcardSet> result_set = new ArrayList<FlashcardSet>();
+        // populate result_set with all possible flashcards from database
 
-        // return presenter.prepareSuccessView()
+
+        // search algorithm
+        ArrayList<String> tags = requestModel.getTags();
+        String input = requestModel.getSearch_input();
+
+
+
+        if (result_set.size() > 0){
+            // return presenter.prepareSuccessView()
+        }
+
+        return presenter.prepareFailView("No Flashcard Sets matched your search criteria, please try again.");
+
     }
 }
