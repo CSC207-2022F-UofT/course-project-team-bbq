d56ce11136153ce17e18987f846a45efea1a30e1
topazerino <47570422+topazerino@users.noreply.github.com>
2022-11-14 13:32:53 -0500

    updated use case classes

--- src/main/java/quiz_settings_use_case/QuizSettingsController.java
+++ src/main/java/quiz_settings_use_case/QuizSettingsController.java
@@ -1,4 +1,17 @@
 package quiz_settings_use_case;
 
 public class QuizSettingsController {
+    private final QuizSettingsInputBoundary quizSettingsInput;
+
+    public QuizSettingsController(QuizSettingsInputBoundary quizSettingsInput) {
+        this.quizSettingsInput = quizSettingsInput;
+    }
+
+    QuizSettingsResponseModel create(int numQuestions, boolean timerOn, int timerDuration,
+                                     boolean multipleChoiceOn, boolean textEntryOn, boolean trueFalseOn) {
+        QuizSettingsRequestModel requestModel = new QuizSettingsRequestModel(numQuestions, timerOn, timerDuration,
+                multipleChoiceOn, textEntryOn, trueFalseOn);
+
+        return quizSettingsInput.create(requestModel);
+    }
 }
