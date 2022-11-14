ea2aac1fe0fa4d459622101e162dffa31a87cf40
Winston Chieng <winston.chieng03@gmail.com>
2022-11-13 19:16:16 -0500

    Implemented Search Algorithm using Database Access

--- src/main/java/search_use_case/SearchInteractor.java
+++ src/main/java/search_use_case/SearchInteractor.java
@@ -1,8 +1,12 @@
 package search_use_case;
 
-import entities.FlashcardSet;
+import dataAccess.*;
+import entityRequestModels.CommonUserDsRequestModel;
+import entityRequestModels.FlashcardSetDsRequestModel;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 
 public class SearchInteractor implements SearchInputBoundary{
 
@@ -14,20 +18,48 @@ public class SearchInteractor implements SearchInputBoundary{
     }
 
     @Override
-    public SearchResponseModel create(SearchRequestModel requestModel){
+    public SearchResponseModel create(SearchRequestModel requestModel) throws IOException {
 
-        ArrayList<FlashcardSet> result_set = new ArrayList<FlashcardSet>();
+        ArrayList<FlashcardSetDsRequestModel> result_set = new ArrayList<>();
+        ArrayList<Integer> flashcard_set_ids = new ArrayList<>();
+
         // populate result_set with all possible flashcards from database
+        IFlashcardSetDataAccess flashcardSetDataAccess = new FlashcardSetDataAccess(DBGateway.getFlashcardSetPath());
+        IUserDataAccess userDataAccess = new CommonUserDataAccess(DBGateway.getUserPath());
+        DBGateway dbGateway = new DBGateway(null, flashcardSetDataAccess, userDataAccess);
 
+        // change to dbGateway.getAllUsers()
+        Collection<CommonUserDsRequestModel> all_users = userDataAccess.getAllUsers();
+        for (CommonUserDsRequestModel user : all_users){
+            flashcard_set_ids.addAll(user.getFlashcardSetIds());
+        }
 
         // search algorithm
         ArrayList<String> tags = requestModel.getTags();
         String input = requestModel.getSearch_input();
+        for (Integer x : flashcard_set_ids){
+            if (!dbGateway.getFlashcardSet(x).getIsPrivate()){
+                for(String tag : tags){
+                    if(tag.equals("Title") && dbGateway.getFlashcardSet(x).getTitle().equals(input)){
+                        result_set.add(flashcardSetDataAccess.getFlashcardSet(x));
+                        break;
+                    }
+                    if(tag.equals("Description") && dbGateway.getFlashcardSet(x).getDescription().equals(input)){
+                        result_set.add(flashcardSetDataAccess.getFlashcardSet(x));
+                        break;
+                    }
+                    if(tag.equals("Owner") && dbGateway.getFlashcardSet(x).getOwnerUsername().equals(input)){
+                        result_set.add(flashcardSetDataAccess.getFlashcardSet(x));
+                        break;
+                    }
+                }
+            }
 
-
+        }
 
         if (result_set.size() > 0){
-            // return presenter.prepareSuccessView()
+            SearchResponseModel searchResponseModel = new SearchResponseModel(result_set);
+            return presenter.prepareSuccessView(searchResponseModel);
         }
 
         return presenter.prepareFailView("No Flashcard Sets matched your search criteria, please try again.");
