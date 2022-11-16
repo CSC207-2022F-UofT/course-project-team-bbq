35215a1665b326168f7f967292eec2ca484a9930
Thomas Shim <thomas.shim@mail.utoronto.ca>
2022-11-16 18:18:17 -0500

    Delete all files that are not related to dataclass-implementation

--- src/main/java/Editor/Flashcard/screens/FlashcardEditFailed.java
+++
@@ -1,7 +0,0 @@
-package Editor.Flashcard.screens;
-
-public class FlashcardEditFailed extends RuntimeException{
-    public FlashcardEditFailed(String error){
-        super(error);
-    }
-}
--- src/main/java/Editor/FlashcardSet/FCSetEditorOutputBoundary.java
+++
@@ -1,7 +0,0 @@
-package Editor.FlashcardSet;
-
-public interface FCSetEditorOutputBoundary {
-    FCSetEditorResponseModel prepareSuccessView(FCSetEditorResponseModel responseModel);
-
-    FCSetEditorResponseModel prepareFailView(String error);
-}
