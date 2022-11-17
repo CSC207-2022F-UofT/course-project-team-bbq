34abe8c0ccf14e4a8f987e75b0e7578be77814f1
prate <lucas.prates@mail.utoronto.ca>
2022-11-17 15:30:32 -0500

    added documentation to StudySessionController

--- src/main/java/studyMode/StudySessionController.java
+++ src/main/java/studyMode/StudySessionController.java
@@ -17,10 +17,24 @@ public class StudySessionController {
         this.inputBoundary = inputBoundary;
     }
 
+    /**
+     * @param userInput tells the system if the user wants to flip the flashcard
+     *                  or go to the next / previous flashcard
+     * @return a data structure containing the new text to display on the flashcard,
+     * as well as the current card number.
+     */
     public StudySessionResponseModel study(StudySessionRequestModel userInput){
         return inputBoundary.study(userInput);
     }
 
+    /**
+     * @param request tells the system the flashcard set the user wants to study,
+     *                how the cards should be sorted, and whether the term or description
+     *                of the flashcards should be displayed first by default
+     * @return a data structure which tells the system if the flashcard set was successfully
+     * fetched from the database, the length and title of the flashcard set, and initial text
+     * to be written to the first flashcard set.
+     */
     public StudySettingsResponseModel getSetToStudy(StudySettingsRequestModel request) {
         return inputBoundary.getSetToStudy(request);
     }
