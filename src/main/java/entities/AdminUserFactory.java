acc8b469b276706c91b0b755dee605735c01ecf1
prate <lucas.prates@mail.utoronto.ca>
2022-11-18 15:00:32 -0500

    deleted AdminUser.java and AdminUserFactory.java

--- src/main/java/entities/AdminUserFactory.java
+++
@@ -1,9 +0,0 @@
-package entities;
-
-public class AdminUserFactory implements UserFactory {
-
-    @Override
-    public User create(String username, String password) {
-        return new AdminUser(username, password);
-    }
-}
