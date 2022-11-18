b9abb10c7fe2ec51d4537c2722fb6f1fe6006b80
topazerino <47570422+topazerino@users.noreply.github.com>
2022-11-18 01:00:14 -0500

    re-adding flatlaf gui themes.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import com.formdev.flatlaf.FlatIntelliJLaf;
+import com.formdev.flatlaf.FlatDarculaLaf;
 
 import quizUseCase.*;
 import dataAccess.*;
@@ -6,9 +7,14 @@ import quizUseCase.screens.QuizSettingsScreen;
 
 import java.io.IOException;
 
+/**
+ * Run the main class to run the application.
+ */
 public class Main {
     public static void main(String[] args) throws IOException {
-        FlatIntelliJLaf.setup();
+        // ONLY UNCOMMENT ONE THEME!!
+        // FlatIntelliJLaf.setup(); // light mode
+        FlatDarculaLaf.setup(); // dark mode
 
         // DATABASE
         IFlashcardDataAccess flashcardGateway = new FlashcardDataAccess(DBGateway.getFlashcardPath());
