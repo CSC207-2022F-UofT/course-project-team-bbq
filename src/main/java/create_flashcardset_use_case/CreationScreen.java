c5e20b7f1b25edab60df316e5035156468431579
Edward-1117 <edward.ishii@mail.utoronto.ca>
2022-11-19 18:09:12 -0500

    Added feature: Create Flashcard Set
    This feature is implemented following the Clean Architecture model.
    
    The GUI is implemented using the Swing library.

---
+++ src/main/java/create_flashcardset_use_case/CreationScreen.java
@@ -0,0 +1,104 @@
+package create_flashcardset_use_case;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Objects;
+
+// Frameworks/Drivers (Blue) layer
+
+public class CreationScreen extends JPanel implements ActionListener {
+    /**
+     * The title of the flashcard set chosen by the user
+     */
+    JTextField title = new JTextField(15);
+    /**
+     * The description of the flashcard set
+     */
+    JTextField description = new JTextField(15);
+    /**
+     * The owner's username of the flashcard set
+     */
+    JTextField username = new JTextField(15);
+
+    /**
+     * The controller
+     */
+    FlashcardSetController flashcardSetController;
+
+    /**
+     * The application
+     */
+    JFrame application;  // need this variable to only close the application and not the whole program
+
+    private boolean privateSelected;  // for public or private status of flashcard set
+
+    /**
+     * A window with a title and a JButton.
+     */
+    public CreationScreen(FlashcardSetController controller, JFrame application) {
+
+        this.flashcardSetController = controller;
+        this.application = application;
+
+        JLabel name = new JLabel("Flashcard Set Creation Screen");
+        name.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        LabelTextPanel flashcardSetTitle = new LabelTextPanel(
+                new JLabel("Title:"), title);
+        LabelTextPanel flashcardSetDescription = new LabelTextPanel(
+                new JLabel("Description:"), description);
+        LabelTextPanel flashcardSetOwner = new LabelTextPanel(
+                new JLabel("Owner (username):"), username);
+
+        JButton signUp = new JButton("Create");
+        JButton cancel = new JButton("Cancel");
+
+        JPanel confirmationButtons = new JPanel();
+        confirmationButtons.add(signUp);
+        confirmationButtons.add(cancel);
+
+        signUp.addActionListener(this);
+        cancel.addActionListener(this);
+
+        JCheckBox privacy = new JCheckBox("Set as Private");
+        JPanel privacyButton = new JPanel();
+        privacyButton.add(privacy);
+        privacy.addActionListener(this);
+
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        this.add(name);
+        this.add(flashcardSetTitle);
+        this.add(flashcardSetDescription);
+        this.add(flashcardSetOwner);
+        this.add(privacy);
+        this.add(confirmationButtons);
+    }
+
+    /**
+     * React to a button click that results in evt.
+     */
+    public void actionPerformed(ActionEvent evt) {
+//        System.out.println("Click " + evt.getActionCommand());
+
+        // Exit the creation screen if user cancels creation
+        if (Objects.equals(evt.getActionCommand(), "Cancel")) {
+            application.dispose();
+        } else if (Objects.equals(evt.getActionCommand(), "Set as Private")) {
+            privateSelected = !privateSelected;  // toggle every time clicked
+        } else {
+            try {
+                flashcardSetController.create(title.getText(), description.getText(),
+                        privateSelected, username.getText(), evt.getActionCommand());
+                JOptionPane.showMessageDialog(this,
+                        String.format("Flashcard Set: [%s] created.", title.getText()));
+                application.dispose();  // exit creation screen
+            } catch (FlashcardSetCreationFailed e) {
+                JOptionPane.showMessageDialog(this, e.getMessage());
+
+            }
+        }
+    }
+}
---
+++ src/main/java/create_flashcardset_use_case/FlashcardSetRequestModel.java
@@ -0,0 +1,56 @@
+package create_flashcardset_use_case;
+
+// Use case (Red) layer
+
+public class FlashcardSetRequestModel {
+    private String title;
+    private String description;
+    private boolean isPrivate;
+    private String username;
+    private String action;  // e.g., when user clicks on "discard" button, action sends "discard" string
+
+    public FlashcardSetRequestModel(String title, String description, boolean isPrivate, String username,
+                                    String action) {
+        this.title = title;
+        this.description = description;
+        this.isPrivate = isPrivate;
+        this.username = username;
+        this.action = action;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+
+    public void setPrivate(boolean aPrivate) {
+        isPrivate = aPrivate;
+    }
+
+    public String getUsername() {return username;}
+
+    public void setUsername(String username) {this.username = username; }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+}
