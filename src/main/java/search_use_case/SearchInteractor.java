497d296037a0168950ac4616383e7c8df0fda5de
Winston Chieng <winston.chieng03@gmail.com>
2022-11-19 11:25:26 -0500

    Added gateway input outside of Interactor

--- src/main/java/search_use_case/SearchInteractor.java
+++ src/main/java/search_use_case/SearchInteractor.java
@@ -10,11 +10,13 @@ import java.util.Collection;
 
 public class SearchInteractor implements SearchInputBoundary{
 
-    final SearchPresenter presenter;
+    private final SearchPresenter presenter;
+    private final DBGateway gateway;
 
     // figure out what else needs to be here
-    public SearchInteractor(SearchPresenter presenter){
+    public SearchInteractor(SearchPresenter presenter, DBGateway gateway){
         this.presenter = presenter;
+        this.gateway = gateway;
     }
 
     @Override
@@ -24,13 +26,10 @@ public class SearchInteractor implements SearchInputBoundary{
         ArrayList<Integer> flashcard_set_ids = new ArrayList<>();
 
         // populate result_set with all possible flashcards from database
-        IFlashcardSetDataAccess flashcardSetDataAccess = new FlashcardSetDataAccess(DBGateway.getFlashcardSetPath());
-        IUserDataAccess userDataAccess = new CommonUserDataAccess(DBGateway.getUserPath());
-        DBGateway dbGateway = new DBGateway(null, flashcardSetDataAccess, userDataAccess);
         CommonUserDsRequestModel curr_user = requestModel.getUser();
 
         // change to dbGateway.getAllUsers()
-        Collection<CommonUserDsRequestModel> all_users = userDataAccess.getAllUsers();
+        Collection<CommonUserDsRequestModel> all_users = gateway.getUserGateway().getAllUsers();
         for (CommonUserDsRequestModel user : all_users){
             flashcard_set_ids.addAll(user.getFlashcardSetIds());
         }
@@ -40,25 +39,25 @@ public class SearchInteractor implements SearchInputBoundary{
         String input = requestModel.getSearch_input();
         if (input.equals("GET_ALL")) {
             for (Integer x : flashcard_set_ids) {
-                if (!dbGateway.getFlashcardSet(x).getIsPrivate()|| curr_user.getIsAdmin()) {
-                    result_set.add(flashcardSetDataAccess.getFlashcardSet(x));
+                if (!gateway.getFlashcardSet(x).getIsPrivate()|| curr_user.getIsAdmin()) {
+                    result_set.add(gateway.getFlashcardSetGateway().getFlashcardSet(x));
                 }
             }
         }
         else{
             for (Integer x : flashcard_set_ids){
-                if (!dbGateway.getFlashcardSet(x).getIsPrivate() || curr_user.getIsAdmin()){
+                if (!gateway.getFlashcardSet(x).getIsPrivate() || curr_user.getIsAdmin()){
                     for(String tag : tags){
-                        if(tag.equals("Title") && dbGateway.getFlashcardSet(x).getTitle().equals(input)){
-                            result_set.add(flashcardSetDataAccess.getFlashcardSet(x));
+                        if(tag.equals("Title") && gateway.getFlashcardSet(x).getTitle().equals(input)){
+                            result_set.add(gateway.getFlashcardSetGateway().getFlashcardSet(x));
                             break;
                         }
-                        if(tag.equals("Description") && dbGateway.getFlashcardSet(x).getDescription().equals(input)){
-                            result_set.add(flashcardSetDataAccess.getFlashcardSet(x));
+                        if(tag.equals("Description") && gateway.getFlashcardSet(x).getDescription().equals(input)){
+                            result_set.add(gateway.getFlashcardSetGateway().getFlashcardSet(x));
                             break;
                         }
-                        if(tag.equals("Owner") && dbGateway.getFlashcardSet(x).getOwnerUsername().equals(input)){
-                            result_set.add(flashcardSetDataAccess.getFlashcardSet(x));
+                        if(tag.equals("Owner") && gateway.getFlashcardSet(x).getOwnerUsername().equals(input)){
+                            result_set.add(gateway.getFlashcardSetGateway().getFlashcardSet(x));
                             break;
                         }
                     }
