c823ac32f9e6fe4abf3ac189715b500f76fccc82
aryans <aryan.chablani@mail.utoronto.ca>
2022-11-21 09:35:39 -0500

    Completed Documentation for Enterprise Business Rules

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,6 +1,13 @@
 package entities;
 import java.util.*;
 
+/**
+ * A user interface that has their username, password, whether they have admin access, and the list of all the ids
+ * of the flashcard sets that belong to them
+ *<p>
+ * Enterprise Business Rules
+ * @author Aryan Chablani
+ */
 public interface User {
     String username = null;
 
@@ -15,12 +22,20 @@ public interface User {
 
     String getPassword();
 
-
-    boolean passwordIsValid();
-
     boolean getIsAdmin();
 
     ArrayList<Integer> getFlashcardSetIds();
 
+    /**
+     * Checks whether the password is valid by not being nothing and greater in length by 5
+     * @return whether the password is valid
+     */
+    boolean passwordIsValid();
+
+    /**
+     * Checks whether the admin key entered is similar to the actual admin key in the business rules
+     * @param adminKey the username of the use
+     * @return whether the admin is the same as the one stored in the enterprise rules
+     */
     boolean adminKeyValid(String adminKey);
 }
