86a700d83bc851dcf1e9bef023686651465f2567
aryans <aryan.chablani@mail.utoronto.ca>
2022-11-21 11:03:44 -0500

    Completed Documentation for UserLoginRequestModel and UserLoginResponseModel

--- src/main/java/loginAndSignupUseCase/UserLoginResponseModel.java
+++ src/main/java/loginAndSignupUseCase/UserLoginResponseModel.java
@@ -2,6 +2,11 @@ package loginAndSignupUseCase;
 
 import java.util.HashMap;
 
+/**
+ * Login Response Model to return to the system for the user
+ * Application Business Rules
+ * @author Aryan Chablani
+ */
 public class UserLoginResponseModel {
     String username;
 
@@ -9,17 +14,25 @@ public class UserLoginResponseModel {
 
     HashMap<Integer, String[]> flashcardSets;
 
+    /**
+     * Constructs a quiz response model based on the following parameters.
+     * @param loggedInUsername the username of the current user using the system
+     * @param isAdmin the admin access of the current user
+     * @param flashcardSets the flashcard sets pertaining to the user obtained by the login interactor
+     */
     public UserLoginResponseModel(String loggedInUsername, boolean isAdmin, HashMap<Integer, String[]> flashcardSets){
         this.username = loggedInUsername;
         this.isAdmin = isAdmin;
         this.flashcardSets = flashcardSets;
     }
 
+    /**
+     * GETTERS for the system
+     */
     public String getSignedInUsername() {
         return username;
     }
     public boolean getIsAdmin(){return isAdmin;}
-
     public HashMap<Integer, String[]> getFlashcardSets(){
         return flashcardSets;
     }
