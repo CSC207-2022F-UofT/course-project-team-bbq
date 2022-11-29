ad04471c746cd98bb804eed694a19acf4ae8f568
Edward-1117 <edward.ishii@mail.utoronto.ca>
2022-11-29 12:20:07 -0500

    Added Javadocs for all classes and methods pertaining to deleting a flashcard set use case.

--- src/main/java/delete_flashcard_set_use_case/DelFlashcardSetOutputBoundary.java
+++ src/main/java/delete_flashcard_set_use_case/DelFlashcardSetOutputBoundary.java
@@ -2,9 +2,27 @@ package delete_flashcard_set_use_case;
 
 // Use case (Red) layer
 
+/**
+ * The output boundary interface that updates the view when deleting a flashcard set succeeds or fails.
+ *
+ * @author Edward Ishii
+ */
 public interface DelFlashcardSetOutputBoundary {
 
+    /**
+     * Prepares the success view when flashcard set deletion succeeds.
+     *
+     * @param message the confirmation of deletion message.
+     * @return the response model containing the deletion message.
+     */
     DelFlashcardSetResponseModel prepareSuccessView(String message);
 
+    /**
+     * Prepares the failure view when flashcard set deletion fails.
+     *
+     * @param error the error message.
+     * @return the response model containing the error message.
+     * @throws FlashcardSetNotFound the error thrown when the flashcard set to be deleted is not found in the database.
+     */
     DelFlashcardSetResponseModel prepareFailView(String error) throws FlashcardSetNotFound;
 }
--- src/main/java/delete_flashcard_set_use_case/DelFlashcardSetRequestModel.java
+++ src/main/java/delete_flashcard_set_use_case/DelFlashcardSetRequestModel.java
@@ -2,18 +2,38 @@ package delete_flashcard_set_use_case;
 
 // Use case (Red) layer
 
+/**
+ * The request model required for deleting a flashcard set.
+ *
+ * @author Edward Ishii
+ */
 public class DelFlashcardSetRequestModel {
 
     private int flashcardSetId;
 
+    /**
+     * Constructs the request model.
+     *
+     * @param flashcardSetId the id of the flashcard set to be deleted.
+     */
     public DelFlashcardSetRequestModel(int flashcardSetId) {
         this.flashcardSetId = flashcardSetId;
     }
 
+    /**
+     * Getter for the flashcard set id.
+     *
+     * @return the id of this flashcard set.
+     */
     public int getFlashcardSetId() {
         return flashcardSetId;
     }
 
+    /**
+     * Setter for the flashcard set id.
+     *
+     * @param flashcardSetId the new id of this flashcard set.
+     */
     public void setFlashcardSetId(int flashcardSetId) {
         this.flashcardSetId = flashcardSetId;
     }
