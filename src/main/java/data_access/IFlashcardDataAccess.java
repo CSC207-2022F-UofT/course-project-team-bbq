dacb3d3c67b785e72fec13a06d1713cd80cf4abf
jli <nitsujil3002@gmail.com>
2022-11-29 14:28:32 -0500

    java doc for IFlashcardDataAccess

--- src/main/java/data_access/IFlashcardDataAccess.java
+++ src/main/java/data_access/IFlashcardDataAccess.java
@@ -2,16 +2,29 @@ package data_access;
 
 import data_access.entity_request_models.FlashcardDsRequestModel;
 
-// use case layer
+/**
+ * User Data Access Interface.
+ * Application Business Rules
+ * @author Justin Li
+ */
 
-public interface    IFlashcardDataAccess {
-
-    FlashcardDsRequestModel getFlashcard(Integer flashcardID);
-
+public interface IFlashcardDataAccess {
+    /**
+     * @param flashcardId the id of the flashcard.
+     * @return the flashcard object with the given id.
+     */
+    FlashcardDsRequestModel getFlashcard(Integer flashcardId);
+    /**
+     * @param flashcard the flashcard object that will be created.
+     */
     int saveFlashcard(FlashcardDsRequestModel flashcard);
-
+    /**
+     * @param flashcard the flashcard object that will be edited.
+     */
     void editFlashcard(FlashcardDsRequestModel flashcard);
-
-    void deleteFlashcard(Integer flashcardID);
+    /**
+     * @param flashcardId the id of the flashcard that will be deleted.
+     */
+    void deleteFlashcard(Integer flashcardId);
 
 }
