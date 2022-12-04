93dd0a56032fba37ae72afb4881be807a6e28d10
topazerino <47570422+topazerino@users.noreply.github.com>
2022-12-03 22:14:24 -0500

    color changes, adding a flashcard set, ui fixes

--- src/main/java/frameworks_and_drivers/components/QuestionCard.java
+++ src/main/java/frameworks_and_drivers/components/QuestionCard.java
@@ -16,8 +16,8 @@ public abstract class QuestionCard extends JPanel {
     protected final ImageIcon no = new ImageIcon("src/images/no.png");
 
     protected final Color DARK_GRAY = new Color(41,41,41);
-    protected final Color GREEN = new Color(24, 134, 0);
-    protected final Color RED = new Color(134, 0, 0);
+    protected final Color GREEN = new Color(25, 59, 16);
+    protected final Color RED = new Color(89, 34, 34);
 
     /**
      * Constructs a question card given the question number.
