9bbabb4c6b79fbbf1db9cefae8dc20d86b26d370
prate <lucas.prates@mail.utoronto.ca>
2022-12-06 13:12:24 -0500

    added documentation to FlashcardByDateComparator.java and FlashcardAlphComparator.java

--- src/main/java/entities/comparators/FlashcardAlphComparator.java
+++ src/main/java/entities/comparators/FlashcardAlphComparator.java
@@ -4,8 +4,16 @@ import entities.Flashcard;
 
 import java.util.Comparator;
 
+/**
+ * A class which knows how to compare flashcard's by term
+ */
 public class FlashcardAlphComparator implements Comparator<Flashcard> {
 
+    /**
+     * @param f1 the first flashcard to be compared.
+     * @param f2 the second flashcard to be compared.
+     * @return an integer which indicates which string comes first alphabetically
+     */
     @Override
     public int compare(Flashcard f1, Flashcard f2) {
        String term1 = f1.getTerm();
--- src/main/java/entities/comparators/FlashcardByDateComparator.java
+++ src/main/java/entities/comparators/FlashcardByDateComparator.java
@@ -4,7 +4,15 @@ import entities.Flashcard;
 
 import java.util.Comparator;
 
+/**
+ * A class which knows how to compare flashcards by date
+ */
 public class FlashcardByDateComparator implements Comparator<Flashcard> {
+    /**
+     * @param f1 the first flashcard to be compared.
+     * @param f2 the second flashcard to be compared.
+     * @return an integer which indicates which flashcard was created first
+     */
     @Override
     public int compare(Flashcard f1, Flashcard f2) {
         return f1.getCreationDate().compareTo(f2.getCreationDate());
